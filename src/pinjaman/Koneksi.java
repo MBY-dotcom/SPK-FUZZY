14f781ecca2c04a1e459fe4a7c498e8d4dca69c0
Mokhammad Brian Yusuf <72732482+MBY-dotcom@users.noreply.github.com>
2022-10-04 09:48:06 +0700

    Add files via upload

---
+++ src/pinjaman/Koneksi.java
@@ -0,0 +1,25 @@
+
+package pinjaman;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+
+public class Koneksi {
+    private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost:3306/test"; //url database
+            String user="root"; //user database
+            String pass="root"; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (Exception e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return mysqlconfig;
+    }
+    
+    
+}
